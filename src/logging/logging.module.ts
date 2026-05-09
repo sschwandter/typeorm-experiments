8b646ee07e0120ed822752a3ed8625eaee5b7a74
Stefan Schwandter <s.schwandter@me.com>
2026-05-09 10:54:54 +0200

    docs: add comments explaining transient Logger and INQUIRER token

--- src/logging/logging.module.ts
+++ src/logging/logging.module.ts
@@ -6,9 +6,13 @@ import { INQUIRER } from '@nestjs/core';
   providers: [
     {
       provide: Logger,
+      // Use TRANSIENT scope so that a new Logger instance is created for every injecting class.
+      // This allows each instance to have its own specific context.
       scope: Scope.TRANSIENT,
+      // The INQUIRER token provides access to the class instance that is requesting the Logger.
       inject: [INQUIRER],
       useFactory: (parentClass: object) => {
+        // Automatically set the Logger context to the name of the class where it's injected.
         return new Logger(parentClass?.constructor?.name ?? 'UnknownContext');
       },
     },
